88ef629843f06a03ee10fe2a67b139d453dd1801
mohit000001 <mohitofficial167@gmail.com>
2022-01-15 20:29:45 +0530

    deploy testing

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -7,8 +7,8 @@ function Home() {
        <>
        This is a simple crud application which deal with student data. following are the technical details of project. <br/> <br/> 
        <ul>
-           <li style={{margin:10}}> <b>Frontend Techs </b> : Reactjs, Typescript, AWS, React Router, Matrail UI, React hook form, Axios</li>
-           <li style={{margin:10}}> <b>Backend Techs </b> : Nodejs, AWS, ExpressJs, MongoDB, Mongoss, Typescript</li>
+           <li style={{margin:10}}> <b>Frontend Techs </b> : Reactjs, Typescript, AWS, React Router, Matrail UI, React hook form, Axios ( Hosted on AWS S3 )</li>
+           <li style={{margin:10}}> <b>Backend Techs </b> : Nodejs, AWS, ExpressJs, MongoDB, Mongoss, Typescript ( Hosted on AWS Ec2 )</li>
            <li style={{margin:10}}> <b>Frontend Repository </b> : <a href="https://github.com/mohit000001/Crud-App-Frontend-React" > Github Frontend App</a></li>
            <li style={{margin:10}}> <b>Backend Repository </b> : <a href="https://github.com/mohit000001/Node-Express-CRUD-APP" > Github Backend App</a></li>
        </ul><br/> 
